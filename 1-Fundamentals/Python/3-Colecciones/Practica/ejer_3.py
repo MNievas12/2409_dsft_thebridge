8dc07bfed8267895b290a5c973456b25cc8adfcf
Miguel Nievas <miguel.nievas.fuertes@gmail.com>
2024-09-25 11:28:49 +0200

    W1 D3 sol

---
+++ 1-Fundamentals/Python/3-Colecciones/Practica/ejer_3.py
@@ -0,0 +1,11 @@
+ejer_3_1 = ["Hola", "amigo"]
+ejer_3_2 = ["Que", "tal"]
+ejer_3_results = []
+
+for x in ejer_3_1:
+    print(x)
+    for y in ejer_3_2:
+        print(y)
+        ejer_3_results.append(x + " " + y)
+
+print(ejer_3_results)
---
+++ 1-Fundamentals/Python/3-Colecciones/Practica/ejer_5.py
@@ -0,0 +1,7 @@
+ejer_5 = [3, 20, 3, 47, 19, 3, 29, 45, 67, 78, 90, 3, 3]
+
+for x in ejer_5:
+    if x == 3:
+        ejer_5.remove(x)
+
+print(ejer_5)
