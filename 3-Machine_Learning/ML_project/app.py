48a19092e7771be850bffd15024dd3ffb1405087
Miguel Nievas <miguel.nievas.fuertes@gmail.com>
2024-11-06 11:15:18 +0100

    W7 D3 sol

--- 3-Machine_Learning/ML_project/app.py
+++ 3-Machine_Learning/ML_project/app.py
@@ -9,8 +9,14 @@ st.title('Simple Streamlit App')
 
 option = st.selectbox(
     "Category",
-    ("High", "Medium", "Low"))
+    ("High", "Medium", "Low", "None"))
 
 # pred = model.predict(user_input)
 # Display the user input
 st.write(f'Hello, {option}!')
+
+col1, col2, col3, col4 = st.columns(4)
+col1.metric("Temperature", "70 °F", "1.2 °F")
+col2.metric("Wind", "9 mph", "-8%")
+col3.metric("Humidity", "86%", "4%")
+col4.metric("Paula", "80%", "10%")
