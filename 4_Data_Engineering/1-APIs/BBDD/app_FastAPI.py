6dd8f786a80cec86de36d5442afb87dfd2718362
Miguel Nievas <miguel.nievas.fuertes@gmail.com>
2024-11-25 10:12:03 +0100

    W10 D1

---
+++ 4_Data_Engineering/1-APIs/BBDD/app_FastAPI.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, HTTPException
+from datos_dummy import books
+
+
+app = FastAPI()
+
+@app.get('/')
+async def home():
+    return "Bienvenido a la API de la base de datos de libros"
+
+# Ruta para obtener todos los libros
+@app.get("/v1/books")
+async def get_all_books():
+    return books
+
+# Ruta para obtener información de un libro por su id
+@app.get("/v1/books/{book_id}")
+async def get_book_by_id(book_id: int):
+    results = [book for book in books if book["id"] == book_id]
+    if not results:
+        raise HTTPException(status_code=404, detail="Book not found")
+    return results
+
+# Endpoint para añadir un libro
+
+
+# Endpoint para modificar un libro por su índice
+
+
+# Endpoint para eliminar un libro por su índice
+
+# Ejecutar la aplicación
---
+++ 4_Data_Engineering/1-APIs/BBDD/ejercicio/app_bd.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, requests
+
+
+# 0.Ruta para obtener todos los libros
+
+# 1.Ruta para obtener el conteo de libros por autor ordenados de forma descendente
+
+
+# 2.Ruta para obtener los libros de un autor
+
+
+# 3.Ruta para añadir un libro
